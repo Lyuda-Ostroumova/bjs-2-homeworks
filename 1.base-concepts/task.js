03a94646820fef5dd4908855ae5db57f64b1ed4c
Lyudmila Ostroumova <lyudmila.ostroumova@gmail.com>
2022-02-05 16:09:09 +0300

    Equation function

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,27 @@
+"use strict"
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let x;
+  let x1;
+  let x2;
+  let D = b**2-4*a*c;
+
+  if (D < 0) {
+    arr = [];
+}
+else if (D == 0) {
+  x = -b/(2*a);
+  arr = [x];
+}
+else if (D > 0) {
+  x1 = (-b + Math.sqrt(D) )/(2*a);
+  x2 = (-b - Math.sqrt(D) )/(2*a);
+ arr = [x1, x2];
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+return arr;
 
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
 }
+
+
