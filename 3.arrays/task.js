eeeeaf24e24418fe6af08b984d06763358ece4d1
Lyudmila Ostroumova <lyudmila.ostroumova@gmail.com>
2022-02-07 16:30:10 +0300

    Task funstions updated

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,19 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  result = arr1.every((item) => arr1[item] === arr2[item]);
 
   return result; // boolean
 }
 
+
+
+
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
-
+  resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0)
+  .map((item) => item * 10);
+  
   return resultArr; // array
 }
