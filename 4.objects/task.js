48ef795c445013016eba39631e7d98a0762cfc4a
Lyudmila Ostroumova <lyudmila.ostroumova@gmail.com>
2022-02-10 20:06:00 +0300

    Object 'Student' created

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,38 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+   this.subject = subjectName;
+   
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  
+   if (this.marks === undefined) {
+      this.marks = [mark];
+    } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...rest) {
+    this.marks = [...rest];
+}
+
+Student.prototype.getAverage = function () {
+  let sum = this.marks.reduce((previous, current) => {
+    return previous + current;
+  });
+  return this.average = sum / this.marks.length
+}
+
+Student.prototype.exclude = function (reason) {
+
+  delete this.subject;
+  delete this.marks;
+
+  this.excluded = reason;
+}
